a40f1f815ce74961da14a574bb3ac3ba84b1e23c
Sheridan Gray <sheridan.gray@gmail.com>
2016-10-18 22:27:34 -0700

    migrations

--- db/migrate/20161004232836_create_clothing_items.rb
+++ db/migrate/20161004232836_create_clothing_items.rb
@@ -2,8 +2,8 @@ class CreateClothingItems < ActiveRecord::Migration
   def change
     create_table :clothing_items do |t|
 
-      t.references :wardrobe, index: true, foreign_key: true
-      t.references :clothing_type, index: true, foreign_key: true
+      # t.references :wardrobe, index: true, foreign_key: true
+      # t.references :clothing_type, index: true, foreign_key: true
       t.date :depreciate_on
       t.date :purchased_on
 
